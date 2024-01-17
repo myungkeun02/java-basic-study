7b580ad4abac3c91a39b1b477bf068478dc1399e
myungkeun <audrms3568@gmail.com>
2024-01-18 00:15:31 +0900

    access modifier(2)

---
+++ src/access/ex/Item.java
@@ -0,0 +1,20 @@
+package access.ex;
+
+public class Item {
+    private String productName;
+    private int price;
+    private int quantity;
+
+    public Item(String productName, int price, int quantity){
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    public String getProductName(){
+        return productName;
+    }
+
+    public int getTotalPrice(){
+        return price * quantity;
+    }
+}
